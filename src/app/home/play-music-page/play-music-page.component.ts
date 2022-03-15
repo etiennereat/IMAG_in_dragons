af96db04b6a27eff1b78f5fe530dda39bfebd61e
m2gi-lepasd <dorian.lepas@etu.univ-grenoble-alpes.fr>
2022-03-15 16:12:42 +0100

    PlayMusicPage

---
+++ src/app/home/play-music-page/play-music-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-play-music-page',
+  templateUrl: './play-music-page.component.html',
+  styleUrls: ['./play-music-page.component.scss'],
+})
+export class PlayMusicPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/playlist/playlist-detail/playlist-detail.module.ts
+++ src/app/playlist/playlist-detail/playlist-detail.module.ts
@@ -5,12 +5,14 @@ import { PlaylistDetailRoutingModule } from './playlist-detail-routing.module';
 import { IonicModule } from '@ionic/angular';
 import { CreateMusiqueComponent } from 'src/app/modals/create-musique/create-musique.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { PlayMusicPageComponent } from 'src/app/home/play-music-page/play-music-page.component';
 
 
 @NgModule({
   declarations: [
     PlaylistDetailComponent,
-    CreateMusiqueComponent
+    CreateMusiqueComponent,
+    PlayMusicPageComponent
   ],
   imports: [
     CommonModule,
