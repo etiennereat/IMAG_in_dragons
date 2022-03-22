e3d9580c9d88e4839f7212de15ab613c4611eaac
m2gi-lepasd <dorian.lepas@etu.univ-grenoble-alpes.fr>
2022-03-22 10:28:08 +0100

    c bo

--- src/app/modals/option-modal/option-modal.component.ts
+++ src/app/modals/option-modal/option-modal.component.ts
@@ -2,7 +2,7 @@ import { Playlist } from './../../models/playlist';
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { PlaylistService } from 'src/app/services/playlist.service';
-import { ModalController } from '@ionic/angular';
+import { ModalController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-option-modal',
@@ -13,10 +13,12 @@ export class OptionModalComponent implements OnInit {
 @Input() playlists:Playlist
 
   playlistForm: FormControl
-  ReadOnly = true
+  ReadOnly = "ReadOnly"
 
-  constructor(private fb: FormBuilder, private playlistService: PlaylistService,
-    private modalController: ModalController) {
+  constructor(private fb: FormBuilder,
+    private playlistService: PlaylistService,
+    private modalController: ModalController,
+    private toastController: ToastController) {
       this.playlistForm = this.fb.control('',[Validators.required, Validators.email]);
   }
 
@@ -27,13 +29,28 @@ export class OptionModalComponent implements OnInit {
     this.modalController.dismiss()
   }
 
+  changeMode(){
+    this.ReadOnly == "ReadOnly" ? "ReadAndWrite" : "ReadOnly"
+  }
+
   sharePlaylist(){
-    if(this.ReadOnly == true){
+    if(this.ReadOnly == "ReadOnly"){
       this.playlistService.sharePlaylistReadOnly(this.playlists.id,this.playlistForm.value)
     }
     else{
       this.playlistService.sharePlaylistReadAndWrite(this.playlists.id,this.playlistForm.value)
     }
+    this.dismiss()
+    this.presentToast("Successfully shared playlist")
+  }
+
+  async presentToast(text:string){
+    const toast = await this.toastController.create({
+      message: text,
+      duration: 2000,
+      icon:"checkmark-outline"
+    });
+    toast.present();
   }
 
 }
